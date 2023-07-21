19c6a3e26cda1f61a0724616515e3ddaf038bac9
misha <mrmishj@gmail.com>
2023-07-21 13:25:53 +0300

    v1.1

---
+++ app/src/main/java/com/example/calc/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.calc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        print("Vitya ne loh")
+    }
+
+}
