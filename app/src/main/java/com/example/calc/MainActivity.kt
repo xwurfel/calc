a327281f11eb325c29a436c5af1a6372f5a9228e
misha <mrmishj@gmail.com>
2023-08-07 18:10:45 +0300

    v3

--- app/src/main/java/com/example/calc/MainActivity.kt
+++ app/src/main/java/com/example/calc/MainActivity.kt
@@ -3,25 +3,38 @@ package com.example.calc
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    private var btn : Button? = null
-    private var tvText : TextView? = null
-    @SuppressLint("SetTextI18n")
+    private var tvInput: TextView? = null
+
+    private var decimal = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btn = findViewById(R.id.btn)
 
-        tvText = findViewById(R.id.text)
+        tvInput = findViewById(R.id.tvInput)
 
-        btn?.setOnClickListener{
-            tvText?.text = "${tvText?.text} + \nVitia loh"
-        }
     }
 
+    fun onDigit(view: View) {
+        tvInput?.append((view as Button).text)
 
+    }
+
+    fun onClear(view: View) {
+        tvInput?.text = ""
+    }
+
+    fun onDecimalPoint(view: View) {
+        if(!decimal) {
+            tvInput?.append(".")
+            decimal = true
+        }
+    }
 }
